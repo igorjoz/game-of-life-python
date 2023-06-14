18dfcff5c6b70c74604a9f492283671892cff615
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-06-14 15:32:15 +0200

    develop Game class - handle save & load to/from file
    - context menu handling
    - spawn newly implemented classes

--- game.py
+++ game.py
@@ -2,12 +2,20 @@ from typing import Union, List
 from enum import Enum
 import numpy as np
 
+from cyber_sheep import CyberSheep
+from dandelion import Dandelion
+from grass import Grass
+from guarana import Guarana
+from hogweed import Hogweed
+from nightshade import Nightshade
+from world import World
+
 from antelope import Antelope
+from fox import Fox
 from human import Human
 from sheep import Sheep
 from tortoise import Tortoise
 from wolf import Wolf
-from world import World
 
 from direction import Direction
 from point import Point
@@ -40,10 +48,19 @@ class Game:
 
     def spawn_initial_organisms(self):
         self.create_human()
+
         self.spawn_wolves()
+        self.spawn_foxes()
         self.spawn_sheep()
+        self.spawn_cyber_sheep()
         self.spawn_antelopes()
         self.spawn_tortoises()
+
+        self.spawn_grass()
+        self.spawn_dandelions()
+        self.spawn_guarana()
+        self.spawn_nightshade()
+        self.spawn_hogweed()
 
     def create_human(self):
         position = Point(0, 0)
@@ -56,10 +73,20 @@ class Game:
             wolf = Wolf(self.world)
             self.world.spawn_organism(wolf)
 
+    def spawn_foxes(self):
+        for _ in range(Fox.INITIAL_QUANTITY):
+            fox = Fox(self.world)
+            self.world.spawn_organism(fox)
+
     def spawn_sheep(self):
         for _ in range(Sheep.INITIAL_QUANTITY):
             sheep = Sheep(self.world)
             self.world.spawn_organism(sheep)
+
+    def spawn_cyber_sheep(self):
+        for _ in range(CyberSheep.INITIAL_QUANTITY):
+            cyber_sheep = CyberSheep(self.world)
+            self.world.spawn_organism(cyber_sheep)
 
     def spawn_antelopes(self):
         for _ in range(Antelope.INITIAL_QUANTITY):
@@ -70,6 +97,31 @@ class Game:
         for _ in range(Tortoise.INITIAL_QUANTITY):
             tortoise = Tortoise(self.world)
             self.world.spawn_organism(tortoise)
+
+    def spawn_grass(self):
+        for _ in range(Grass.INITIAL_QUANTITY):
+            grass = Grass(self.world)
+            self.world.spawn_organism(grass)
+
+    def spawn_dandelions(self):
+        for _ in range(Dandelion.INITIAL_QUANTITY):
+            dandelion = Dandelion(self.world)
+            self.world.spawn_organism(dandelion)
+
+    def spawn_guarana(self):
+        for _ in range(Guarana.INITIAL_QUANTITY):
+            guarana = Guarana(self.world)
+            self.world.spawn_organism(guarana)
+
+    def spawn_nightshade(self):
+        for _ in range(Nightshade.INITIAL_QUANTITY):
+            nightshade = Nightshade(self.world)
+            self.world.spawn_organism(nightshade)
+
+    def spawn_hogweed(self):
+        for _ in range(Hogweed.INITIAL_QUANTITY):
+            hogweed = Hogweed(self.world)
+            self.world.spawn_organism(hogweed)
 
     def move_player(self, direction: Direction):
         human = self.world.get_human()
@@ -86,23 +138,14 @@ class Game:
         self.world.take_turn()
         self.increment_organisms_age()
         self.turn += 1
-        #
-        # if self.special_ability_cooldown > 0:
-        #     self.special_ability_cooldown -= 1
-        #
-        # if self.special_ability_duration > 0:
-        #     self.special_ability_duration -= 1
-        #
-        #     if self.special_ability_duration == 0:
-        #         self.special_ability_cooldown = self.SPECIAL_ABILITY_COOLDOWN
-        #
-        # for i in range(self.size):
-        #     for j in range(self.size):
-        #         if self.world.get_organisms()[j][i] is None:
-        #             print("- ", end='')
-        #         else:
-        #             print(self.world.get_organisms()[j][i].get_symbol(), " ", end='')
-        #     print()
+
+        if self.special_ability_cooldown > 0:
+            self.special_ability_cooldown -= 1
+
+        if self.special_ability_duration > 0:
+            self.special_ability_duration -= 1
+            if self.special_ability_duration == 0:
+                self.special_ability_cooldown = self.SPECIAL_ABILITY_COOLDOWN
 
     def increment_organisms_age(self):
         for organism in self.world.get_organisms_list():
@@ -126,11 +169,111 @@ class Game:
         return self.world.get_organisms_list()
 
     def save_to_file(self):
-        with open("save.txt", "w") as file:
-            # Add saving mechanism here, e.g.:
-            file.write(str(self.turn) + "\n")
-            # ...
+        try:
+            with open('save.txt', 'w') as file:
+                file.write(f"{self.turn}\n")
+                file.write(f"{self.size} {len(self.world.organisms_list)}\n")
+                file.write(f"{self.special_ability_cooldown} {self.special_ability_duration}\n")
+                file.write(f"{self.world.human.position.x} {self.world.human.position.y}\n")
 
-    # def load_from_file(self):
-    #     try:
-    #         with open("save.txt", "r")
+                for organism in self.world.organisms_list:
+                    file.write(f"{organism.position.x} {organism.position.y} "
+                               f"{organism.strength} {organism.age} {organism.species}\n")
+
+        except FileNotFoundError as e:
+            print(f"File error: {e}")
+
+    def load_from_file(self):
+        try:
+            with open('save.txt', 'r') as file:
+                self.world.clear_organisms()
+
+                lines = file.readlines()
+
+                self.turn = int(lines[0])
+                self.size = int(lines[1].split()[0])
+                organisms_quantity = int(lines[1].split()[1])
+
+                self.special_ability_cooldown = int(lines[2].split()[0])
+                self.special_ability_duration = int(lines[2].split()[1])
+
+                player_x = int(lines[3].split()[0])
+                player_y = int(lines[3].split()[1])
+
+                self.world.human.position = Point(player_x, player_y)
+
+                for i in range(organisms_quantity):
+                    x = int(lines[4 + i].split()[0])
+                    y = int(lines[4 + i].split()[1])
+                    strength = int(lines[4 + i].split()[2])
+                    age = int(lines[4 + i].split()[3])
+                    species_string = lines[4 + i].split()[4]
+
+                    if species_string == "Human":
+                        organism = Human(self.world, Point(x, y))
+                        self.world.human = organism
+                    elif species_string == "Wolf":
+                        organism = Wolf(self.world, Point(x, y))
+                    elif species_string == "Sheep":
+                        organism = Sheep(self.world, Point(x, y))
+                    elif species_string == "CyberSheep":
+                        organism = CyberSheep(self.world, Point(x, y))
+                    elif species_string == "Fox":
+                        organism = Fox(self.world, Point(x, y))
+                    elif species_string == "Tortoise":
+                        organism = Tortoise(self.world, Point(x, y))
+                    elif species_string == "Antelope":
+                        organism = Antelope(self.world, Point(x, y))
+                    elif species_string == "Grass":
+                        organism = Grass(self.world, Point(x, y))
+                    elif species_string == "Dandelion":
+                        organism = Dandelion(self.world, Point(x, y))
+                    elif species_string == "Guarana":
+                        organism = Guarana(self.world, Point(x, y))
+                    elif species_string == "Nightshade":
+                        organism = Nightshade(self.world, Point(x, y))
+                    elif species_string == "Hogweed":
+                        organism = Hogweed(self.world, Point(x, y))
+
+                    organism.strength = strength
+                    organism.age = age
+
+                    self.world.set_organism(organism, Point(x, y))
+
+        except FileNotFoundError:
+            print("Error: could not open file")
+
+    def spawn_from_menu(self, menu_index, position: Point):
+        menu_items = ["Wolf", "Sheep", "Antelope", "Tortoise", "CyberSheep", "Fox", "Grass", "Guarana", "Dandelion", "Nightshade", "Hogweed"]
+        menu_item = menu_items[menu_index]
+
+        if menu_item == "Wolf":
+            organism = Wolf(self.world)
+        elif menu_item == "Sheep":
+            organism = Sheep(self.world)
+        elif menu_item == "Antelope":
+            organism = Antelope(self.world)
+        elif menu_item == "Tortoise":
+            organism = Tortoise(self.world)
+        elif menu_item == "CyberSheep":
+            organism = CyberSheep(self.world)
+        elif menu_item == "Fox":
+            organism = Fox(self.world)
+        elif menu_item == "Grass":
+            organism = Grass(self.world)
+        elif menu_item == "Guarana":
+            organism = Guarana(self.world)
+        elif menu_item == "Dandelion":
+            organism = Dandelion(self.world)
+        elif menu_item == "Nightshade":
+            organism = Nightshade(self.world)
+        elif menu_item == "Hogweed":
+            organism = Hogweed(self.world)
+
+        position.x //= 40
+        position.y //= 40
+
+        organism.set_position(position)
+        print(position.x, position.y)
+        self.world.set_organism(organism, position)
+        # self.world.spawn_organism(organism)
