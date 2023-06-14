d75bf25344c933007823bda664823334e773ac41
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-06-14 11:34:21 +0200

    create PredatorAnimal & Wold classes

---
+++ wolf.py
@@ -0,0 +1,30 @@
+from predator_animal import PredatorAnimal
+from point import Point
+from species import Species
+
+
+class Wolf(PredatorAnimal):
+    INITIAL_QUANTITY = 2
+    STRENGTH = 9
+    INITIATIVE = 5
+    SYMBOL = 'W'
+
+    def __init__(self, world, position=None):
+        if position:
+            super().__init__(self.STRENGTH, self.INITIATIVE, self.SYMBOL, position, world)
+        else:
+            super().__init__(self.STRENGTH, self.INITIATIVE, self.SYMBOL, Point(0, 0), world)
+        self.species = Species.WOLF
+
+    def reproduce(self, position):
+        free_space = self.world.get_random_free_space_around(position)
+
+        if free_space is None:
+            return
+
+        new_organism = Wolf(self.world, free_space)
+        self.world.set_organism(new_organism, free_space)
+
+        message = f"Organism {self.symbol} reproduced at ({position.x}, {position.y})"
+        print(message)
+        self.world.add_turn_summary_message(message)
